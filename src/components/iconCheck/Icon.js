40ab4e851a42c7deb0eac45d11003b10d4f4a549
Arthut Kelvim <kelvimarthur@gmail.com>
2022-08-08 20:07:34 -0300

    Sessão de planos ideais concluído

---
+++ src/components/iconCheck/Icon.js
@@ -0,0 +1,11 @@
+import iconCheckedPng from "../../assets/check.png"
+import { ImgIcon } from "./styled"
+
+const Ic = () => {
+    return(
+        <>
+            <ImgIcon src={iconCheckedPng} alt="Coberto pelo plano" />
+        </>
+    )
+}
+export default Ic
---
+++ src/components/plans/styled.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+export const ArticTitlePrice = styled.article`
+    width: 80%;
+    margin: 0 auto;
+    border-top: 2px solid #1C5A3D;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const TitlePlans = styled.h4`
+    font-size: 30px;
+    color: #1C5A3D;
+`
+export const PgraphPlans = styled.p`
+    color: #595959;
+`
+export const CardPriceSection = styled.section`
+    width: 80%;
+    margin: 80px auto;
+    display: flex;
+    justify-content: space-between;
+    
+    color: #fff;
+`
+export const TitleCard = styled.h4`
+    text-align: center;
+    font-weight: 300;
+    margin: 0;
+    padding: 0;
+    margin: 30px 20px 5px;
+`
+export const SubTitle = styled.span`
+    color: #668EF5;
+    display: block;
+    text-align: center;
+    font-size: 11px;
+    margin-bottom: 20px;
+`
+export const PriceCard = styled.strong`
+    display: block;
+    color: #fff;
+    text-align: center;
+    font-weight: 300;
+    font-size: 30px;
+`
+export const PerMonth = styled.span`
+    font-size: 12px;
+    text-transform: uppercase;
+    display: block;
+    color: #668EF5;
+    text-align: center;
+    font-weight: bold;
+`
+export const ListBenefits = styled.ul`
+    margin: 30px 20px;
+    padding: 25px 0;
+    list-style: none;
+    font-size: 11px;
+    border-top: 1px solid #fff;
+`
+export const BtnPlan = styled.button`
+    height: 25px;
+    width: 100px;
+    font-size: 10px;
+    border: none;
+    //border-radius: 15px;
+    color: #1C5A3D;
+    text-transform: uppercase;
+    cursor: pointer;
+    /* border-left: 1px solid #668EF5;
+    border-bottom: 1px solid #668EF5; */
+`
+
+
+export const FirstCardPrice = styled.div`
+    background-color: #0D2329;
+    width: 200px;
+`
+export const SecondCardPrice = styled.div`
+    background-color: #B8EC5B;
+    width: 200px;
+`
+export const ThirdCardPrice = styled.div`
+    background-color: #4BE572;
+    width: 200px;
+`
+export const FourthCardPrice = styled.div`
+    background-color: #10CEA0;
+    width: 200px;
+`
